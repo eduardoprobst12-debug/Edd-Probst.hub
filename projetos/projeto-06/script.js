a12d2e8a0a5477d2b22b45ec070197b54cf74ee4
EDUARDO PROBST <eduardo_probst150@estudante.sesisenai.org.br>
2026-08-19 15:52:57 -0300

    Edd-Torami.hub

--- projetos/projeto-06/script.js
+++ projetos/projeto-06/script.js
@@ -29,7 +29,35 @@ function adicionarTarefa() {
         contador++;
         tarefas.push ({id: contadorId, tarefa: texto, concluido: false });
         imputText.value = "";
-        console.log (tarefas);
+        renderizar();
 
  }
+}
+
+function renderizar() {
+
+    lista.innerHTML = "";
+    tarefas.forEach(function(item) {
+
+        let li = document.createElement ("li");
+        li.textContent = item.tarefa;
+        lista.appendChild (li);
+
+        let btnExcluir = document.createElement ("button")
+        btnExcluir.innerText = "❌​"
+        li.appendChild
+        btnExcluir.addEventListener ("click", function(e) {
+
+            e.stopPropagation();
+
+            tarefas = tarefas.filter (function(t) {
+
+                return t.id !== item.id
+
+   })
+
+   renderizar();
+
+  })
+ });
 }
